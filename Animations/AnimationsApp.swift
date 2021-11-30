bdf0f12d8c39fec138fbffd5e56d74f0cec146c5
Casadina <casadina@tuta.io>
2021-11-30 19:11:16 +0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by aeglus on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
